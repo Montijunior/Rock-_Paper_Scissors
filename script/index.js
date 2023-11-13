580b30fdb1f7b608b5cb36edef87757da5ae9199
Monti junior <montiabouou@gmail.com>
2023-11-13 13:34:07 -0800

    fix : fixed the bug that prevented computerSelection() to be evaluated

--- script/index.js
+++ script/index.js
@@ -11,24 +11,24 @@ function getComputerChoice() {
 // playRound(playerSelection,computerSelection) : function that plays a single round of RPS and returns the winner
 
 function playRound(playerSelection,computerSelection) {
-    if (playerSelection === computerSelection) {
-        console.log(`You draw!! ${playerSelection} equals ${computerSelection}.`);
-        // conditionals for player as winner
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        console.log(`You win!! ${playerSelection} beats ${computerSelection}`);
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper'); {
-        console.log(`You win!! ${playerSelection} beats ${computerSelection}`); 
-    }  if (playerSelection === 'paper' && computerSelection === 'rock') {
-        console.log(`You win ${playerSelection} beats ${computerSelection}`);
+    // let's start with computerSelection
+    if (computerSelection === playerSelection) {
+        return console.log(`You draw!! ${computerSelection} equals ${playerSelection}`);
     } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
-        console.log(`YOu loose!! ${computerSelection} beats ${playerSelection}`);
+        return console.log(`You loose!! ${computerSelection} beats ${playerSelection}`);
     } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
-        console.log(`You loose!! ${computerSelection} beats ${playerSelection}`);
-    } else  {
-        console.log(`You loose again!! ${computerSelection} beats ${playerSelection}`);
-    };
+        return console.log(`You loose!! ${computerSelection} beats ${playerSelection}`);
+    } else if (computerSelection === 'paper' && playerSelection === 'rock') {
+        return console.log(`You loose!! ${computerSelection} beats ${playerSelection}`);
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return console.log(`You win!! ${playerSelection} beats ${computerSelection}`);
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return console.log(`You win!! ${playerSelection} beats ${computerSelection}`);
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return console.log(`You win!! ${playerSelection} beats ${computerSelection}`);
+    }
 }
-
 // create the playerSelection input and should case insensitive
 
 const playerSelection1 = String(prompt('Select either Rock,Paper or Scissors'));
@@ -38,3 +38,8 @@ const playerSelection = playerSelection1.toLowerCase();
 const computerSelection = getComputerChoice();
 // console.log(computerSelection); working
 // console.log(playerSelection); working
+
+
+console.log(playRound(playerSelection,computerSelection));
+
+// game function for the number of rounds
