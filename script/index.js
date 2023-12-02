e0c850d3775219a72273277bb9e3bcbb3546ae01
Monti junior <montiabouou@gmail.com>
2023-12-02 01:59:23 -0800

    fix : fixed the bug that prevented computerSelection() to be evaluated

--- script/index.js
+++ script/index.js
@@ -1,30 +1,59 @@
-//Random computer choice
-function getComputerChoice() {
-    let values = ['rock','paper','scissors'];
-    let random = Math.floor((Math.random() * values.length));
-    let choice = values[random];
-    return choice;
+let section = document.querySelector('section');
+let buttons = document.querySelectorAll('button');
+let scorePlayer = document.querySelector('.P-score');
+let scoreComputer = document.querySelector('.C-score');
+
+let resultText = document.createElement('div');
+resultText.classList.add('roundResult');
+section.insertBefore(resultText,section.lastElementChild);
+
+buttons.forEach((button)=>{
+    button.addEventListener('click',()=>{
+       let round = playRound(button.id,getComputerChoice());
+        resultText.textContent = round;
+    })
+});
+
+
+
+
+function getComputerChoice(){
+    let choice = ['Rock','Paper','Scissors'];
+    let random = Math.floor(Math.random() * choice.length);
+    return choice[random];
 }
 
+let playerScore = 0;
+let computerScore = 0;
 
 
 
 function playRound(playerSelection,computerSelection) {
-    // let's start with computerSelection
-    if (computerSelection === playerSelection) {
-        return `You draw!! ${computerSelection} equals ${playerSelection}`;
-    } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
-        return console.log(`You loose!! ${computerSelection} beats ${playerSelection}`);
-    } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
-        return `You loose!! ${computerSelection} beats ${playerSelection}`;
-    } else if (computerSelection === 'paper' && playerSelection === 'rock') {
-        return `You loose!! ${computerSelection} beats ${playerSelection}`;
+    for ( let i = 0; i <= 5; i++) {
+    if (playerSelection === computerSelection) {
+        // resultText.textContent = round;
+        resultText.style.color = 'white';
+        resultText.style.backgroundColor = 'black';
+        return `Tie !! ${playerSelection} equals ${computerSelection}`;
+
+    } else if (
+        (playerSelection === 'Rock' && computerSelection === 'Scissors') || 
+        (playerSelection === 'Scissors' && computerSelection === 'Paper') || 
+        (playerSelection === 'Paper' && computerScore === 'Rock')) {
+            playerScore++;
+            scorePlayer.textContent = playerScore;
+            resultText.style.backgroundColor = 'green';
+            resultText.style.color = 'white';
+            return `You win !! ${playerSelection} beats ${computerSelection}.`
+        } else {
+            computerScore++;
+            scoreComputer.textContent = computerScore;
+            resultText.style.backgroundColor = 'red';
+            resultText.style.color = 'white';
+            return `You loose !! ${computerSelection} beats ${playerSelection}.`
+
+        }
+    
     }
-    else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return `You Win !! ${playerSelection} beats ${computerSelection}`;
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return `You Win !! ${playerSelection} beats ${computerSelection}`;
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return `You Win !! ${playerSelection} beats ${computerSelection}`;
-    }
+
 }
